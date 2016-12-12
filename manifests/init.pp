627d8105c3205b14f5c021deafd2ceea8292ece3
tspeigner <tspeigner@gmail.com>
2016-12-12 14:55:49 -0800

    closer

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,11 +3,6 @@ class makevm {
   ensure                      => 'present',
   source                      => '/west1/vm/windows-2012-eso',
   cpus                        => '1',
-  #datacenter                  => 'west1',
   memory                      => '2048',
-  #power_state                 => 'poweredOff',
-  #resource_pool               => '/tse1',
-  tools_installer_mounted     => 'true',
-  #hostname                    => 'tls.puppetlabs.vm',
   }
 }
