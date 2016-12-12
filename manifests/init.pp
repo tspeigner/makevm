32e3908e037b5dff912b117691824a900976633f
tspeigner <tspeigner@gmail.com>
2016-12-12 14:54:24 -0800

    which one works?

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,7 +5,7 @@ class makevm {
   cpus                        => '1',
   #datacenter                  => 'west1',
   memory                      => '2048',
-  power_state                 => 'poweredOff',
+  #power_state                 => 'poweredOff',
   #resource_pool               => '/tse1',
   tools_installer_mounted     => 'true',
   #hostname                    => 'tls.puppetlabs.vm',
