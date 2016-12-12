6d2dbd5833985347ef17c61187e4be7e643141cd
tspeigner <tspeigner@gmail.com>
2016-12-12 14:46:08 -0800

    commented out a couple of things

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,13 +1,13 @@
 class makevm {
-  vsphere_vm { '/vm/tlsvsphere':
+  vsphere_vm { '/west1/vm/tlsvsphere':
   ensure                      => 'present',
   source                      => '/west1/vm/windows-2012-eso',
   cpus                        => '1',
   #datacenter                  => 'west1',
   memory                      => '2048',
   power_state                 => 'poweredOff',
-  resource_pool               => '/tse1',
+  #resource_pool               => '/tse1',
   tools_installer_mounted     => 'true',
-  hostname                    => 'tls.puppetlabs.vm',
+  #hostname                    => 'tls.puppetlabs.vm',
   }
 }
