5a97f6c75804a26adb77801a02de70a677b03bc9
tspeigner <tspeigner@gmail.com>
2016-12-12 14:40:28 -0800

    changed path

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,11 +1,11 @@
 class makevm {
-  vsphere_vm { '/west1/vm/tls vsphere test':
-  ensure                      => 'running',
+  vsphere_vm { '/vm/tlsvsphere':
+  ensure                      => 'present',
   source                      => '/west1/vm/windows-2012-eso',
   cpus                        => '1',
-  datacenter                  => 'west1',
+  #datacenter                  => 'west1',
   memory                      => '2048',
-  #power_state                 => 'poweredOff',
+  power_state                 => 'poweredOff',
   resource_pool               => '/tse1',
   tools_installer_mounted     => 'true',
   hostname                    => 'tls.puppetlabs.vm',
