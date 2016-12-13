d095a50f2dbd0aafa052fc1ee4e481412c06a421
tspeigner <tspeigner@gmail.com>
2016-12-13 11:59:51 -0800

    changed vm name

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,5 @@
 class makevm {
-  vsphere_vm { '/west1/vm/TSEs/tommy/vspheretest':
+  vsphere_vm { '/west1/vm/TSEs/tommy/vspheretest1':
   ensure        => running,
   source        => '/west1/vm/Templates/windows-2012-updatedtools',
   resource_pool => '/west1/tse1',
